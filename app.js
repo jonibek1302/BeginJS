aef12ff040ee54ed87a5bb7bfb116f71db0ecdf8
jonibek <jonibekmirzaaliyev2002@gmail.com>
2022-11-18 23:42:33 +0500

    JS ishlash

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
+alert("Assalomu alaykum saytimizga kirishingizdan oldin savollarga javob bersangiz!");
 var username = prompt("Ismingizni kiriting");
-console.log('Ismi:' + username);
+console.log("Ismi:" + username);
+var usersname = prompt("Familiyangizni kiriting");
+console.log("Familiyasi:" + username);
 var userage = prompt("Yoshingizni kiriting");
-console.log("Yoshi" + userage);
+console.log("Yoshi:" + userage);
 var userjob = prompt("Kasbingizni kiriting");
 console.log("Kasbi:" + userjob);
-alert("Javob berganiz uchun raxmat!")
+alert("Javob berganiz uchun raxmat!");
 var user = {
-    name:username,
-    age:userage,
-    job:userjob,
-
-}
+  name: username,
+  sname: usersname,
+  age: userage,
+  job: userjob,
+};
 var use = {
-    add: "Yaxshi inson 😊 "
-}
- console.log(user);
+  add: "Saytimizga tashrif buyurganiz uchun raxmat 😊 ",
+};
+console.log(user);
 document.querySelector(".nameh2").innerText = user["name"];
+document.querySelector(".snameh2").innerText = user["sname"];
 document.querySelector(".ageh2").innerText = user["age"];
 document.querySelector(".jobh2").innerText = user["job"];
 document.querySelector(".addd").innerText = use["add"];
